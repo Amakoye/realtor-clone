26b6d35b1808f94206ca9709ebc3e863cdec46aa
Amakoye <charlesamakoye@gmail.com>
2023-01-11 15:10:55 +0300

    create-pages and routes

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-2xl bg-red-200">Hello world</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
